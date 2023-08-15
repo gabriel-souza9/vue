91c6c874bdde26a794cbf11ccd213b6f5c9d09d0
gabriel <gabriel.souza@interativachat.com.br>
2023-08-15 15:00:13 -0300

    primeiro component vue

---
+++ app_vue/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <PrimeiroComponent />
+</template>
+
+<script>
+import PrimeiroComponent from './components/PrimeiroComponent.vue';
+  export default {
+    name: 'App',
+    components: {
+      PrimeiroComponent
+    }
+  }
+</script>
