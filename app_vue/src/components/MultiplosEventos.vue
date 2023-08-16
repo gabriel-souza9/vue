6092154e6c66a44786ace244c4693701be643e11
gabriel <gabriel.souza@interativachat.com.br>
2023-08-16 17:13:42 -0300

    multiplos eventos

---
+++ app_vue/src/components/MultiplosEventos.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="primeiro(txt1), segundo(txt2)">Ativar múltiplos eventos</button>
+        <p>{{ multiplos_eventos }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MultiplosEventos",
+        data(){
+            return {
+                multiplos_eventos: "",
+                txt1: "Primeiro evento",
+                txt2: "Segundo evento"
+            }
+        },
+        methods: {
+            primeiro(text, e){
+                setTimeout(() => {
+                    this.multiplos_eventos = text;
+                }, 500)
+            },
+            segundo(text, e){
+                setTimeout(() => {
+                    this.multiplos_eventos = text;
+                }, 1000)
+            }
+        }
+    }
+</script>
+
+<style>
+    div {
+        margin: 20px;
+    }
+</style>
--- app_vue/src/components/PrimeiroComponent.vue
+++ app_vue/src/components/PrimeiroComponent.vue
@@ -2,12 +2,17 @@
     <div>
         <h1>Olá Vue!</h1>
         <p>meu nome é {{ name }} e sou {{ proff }}</p>
+        <MultiplosEventos />
     </div>
 </template>
 
 <script>
+import MultiplosEventos from './MultiplosEventos.vue';
     export default {
         name: "PrimeiroCompoment",
+        components: {
+            MultiplosEventos
+        },
         data(){
             return {
                 name: "Souzinha",
